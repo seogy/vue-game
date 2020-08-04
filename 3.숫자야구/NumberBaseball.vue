b8cd14e1cd9346304f3d496608a7fd905ed81cd6
seogy <seogy20@gmail.com>
2020-08-04 17:03:48 +0900

    구구단,  끝말잇기, 웹팩

---
+++ 3.숫자야구/NumberBaseball.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>{{result}}</div>
+    <form v-on:submit="onSubmitForm">
+      <input ref="answer" maxlength="4" v-model="value" />
+      <button>입력</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        value: '',
+        result: '',
+      }
+    },
+    methods: {
+      onSubmitForm(e) {
+        e.preventDefault();
+
+      }
+    }
+  };
+</script>
+
+<style>
+
+</style>
