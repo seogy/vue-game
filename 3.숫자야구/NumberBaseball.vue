aec786e60cda486496dc891c6643c12d68934132
서가영 <gyseo@c2monster.com>
2020-08-05 07:54:49 +0900

    v-for 사용해봄

--- 3.숫자야구/NumberBaseball.vue
+++ 3.숫자야구/NumberBaseball.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
     <div>{{result}}</div>
-    <form v-on:submit="onSubmitForm">
-      <input ref="answer" maxlength="4" v-model="value" />
+    <form @submit.prevent="onSubmitForm">
+      <input type="number" ref="answer" minlength="4" maxlength="4" v-model="value" />
       <button>입력</button>
     </form>
+    <div>시도 {{tries.length}}</div>
+    <ul>
+      <li v-for="t in tries">
+        <div>{{t.try}}</div>
+        <div>{{t.result}}</div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -12,13 +19,20 @@
   export default {
     data() {
       return {
+        answer: geNumbers(),
+        tries: [],
         value: '',
         result: '',
       }
     },
     methods: {
-      onSubmitForm(e) {
-        e.preventDefault();
+      onSubmitForm() { 
+        this.tries.push({
+          try: this.value,
+          result:'홈런'
+          });
+        this.value = '';
+        this.$refs.answer.focus();
 
       }
     }
