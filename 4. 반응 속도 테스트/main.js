ef4d3f262de9f103575e085e8ff71e5b58fde677
seogy <seogy20@gmail.com>
2020-08-07 07:17:37 +0900

    반응속도 체크

---
+++ 4. 반응 속도 테스트/main.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+
+import ResponseCheck from './ResponseCheck';
+
+new Vue(ResponseCheck).$mount('#root');
+
---
+++ 4. 반응 속도 테스트/webpack.config.js
@@ -0,0 +1,32 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // production
+  devtool: 'eval', // hidden-source-map
+  resolve: {
+    extensions: ['.js', '.vue'],
+  },
+  entry: {
+    app: path.join(__dirname, 'main.js'),
+  },
+  module: {
+    rules: [{
+      test: /\.vue$/,
+      use: 'vue-loader'
+      },{
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+        ], 
+      }]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist'),
+  }
+};
